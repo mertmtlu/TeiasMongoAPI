5bfe44adb6cc496d6730152f021959c7f04e9757
mertmtl <mertmtl0109@gmail.com>
2025-05-12 14:32:19 +0300

    delete request and response dtos

--- TeiasMongoAPI.Services/DTOs/Request/RegionUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TeiasMongoAPI.Services.DTOs.Request
-{
-    public class RegionUpdateDto
-    {
-        public List<string>? Cities { get; set; }
-        public string? Headquarters { get; set; }
-    }
-}
