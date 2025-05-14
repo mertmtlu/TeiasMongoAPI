8f9e68ef0a8e6df42ec66f937604c39ae23baf30
mertmtl <mertmtl0109@gmail.com>
2025-05-14 14:41:10 +0300

    fix bugs

---
+++ TeiasMongoAPI.API/Configuration/ValidationConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using TeiasMongoAPI.API.Filters;
+
+namespace TeiasMongoAPI.API.Configuration
+{
+    public static class ValidationConfiguration
+    {
+        public static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            // Add FluentValidation validators from the Services assembly
+            services.AddValidatorsFromAssembly(typeof(Services.Validators.Auth.UserRegisterDtoValidator).Assembly);
+
+            // Configure API behavior
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            // Add custom validation filter
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(ValidationFilter));
+            });
+
+            return services;
+        }
+    }
+}
